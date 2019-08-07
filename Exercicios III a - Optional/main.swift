7fffa43230c0e4b788a6708632c56f81f780d3d7
Alexandre Gerken Brasil <Alexandre Gerken BRasil>
2019-08-07 10:08:39 -0400

    Initial Commit

---
+++ Exercicios III a - Optional/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Exercicios III a - Optional
+//
+//  Created by XCodeClub on 2019-08-07.
+//  Copyright © 2019 Alexandre Gerken Brasil. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
